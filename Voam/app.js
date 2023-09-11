b4880ba308fb2d1aa21a02e31aaf2d656319db7e
Konstantin <kzaraliev@gmail.com>
2023-09-12 00:12:05 +0300

    .

--- Voam/app.js
+++ Voam/app.js
@@ -2,10 +2,11 @@ const background = document.querySelector("#site-background");
 const bttnEnter = document.querySelector("#bttn-enter");
 const main = document.querySelector(".main");
 const mainBody = document.querySelector(".main-body");
-const productBlocks = document.querySelectorAll(".product-block");
+const productBlocks = document.querySelectorAll(".open-popup");
 const body = document.querySelector("body");
 
 bttnEnter.addEventListener("click", enterSite);
+
 productBlocks.forEach((product) => {
   product.addEventListener("click", function () {
     showProduct(product);
@@ -30,8 +31,14 @@ function enterSite(e) {
 }
 
 function showProduct(product) {
+  //access to product-block
+  while (product.classList[0] != "product-block") {
+    product = product.parentElement;
+  }
+
   const container = createElement("div", null, null, "popup-window", body);
-  const img = createElement("img", null, null, "popup-img", container);
+  const slideshow = product.firstElementChild.cloneNode(true);
+  container.appendChild(slideshow);
 
   const contentContainer = createElement(
     "div",
@@ -141,3 +148,44 @@ function createElement(type, textContent, classes, id, parent) {
 
   return element;
 }
+
+//slideshow===============================
+(function () {
+  init();
+
+  function init() {
+    parents = document.getElementsByClassName("slideshow-container");
+
+    for (j = 0; j < parents.length; j++) {
+      var slides = parents[j].getElementsByClassName("mySlides");
+      slides[0].classList.add("active-slide");
+    }
+  }
+
+  links = document.querySelectorAll(".slideshow-container a");
+
+  for (i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+      current = this.parentNode;
+
+      var slides = current.getElementsByClassName("mySlides");
+      curr_slide = current.getElementsByClassName("active-slide")[0];
+      curr_slide.classList.remove("active-slide");
+      if (this.className == "next") {
+        if (curr_slide.nextElementSibling.classList.contains("mySlides")) {
+          curr_slide.nextElementSibling.classList.add("active-slide");
+        } else {
+          slides[0].classList.add("active-slide");
+        }
+      }
+
+      if (this.className == "prev") {
+        if (curr_slide.previousElementSibling) {
+          curr_slide.previousElementSibling.classList.add("active-slide");
+        } else {
+          slides[slides.length - 1].classList.add("active-slide");
+        }
+      }
+    };
+  }
+})();
